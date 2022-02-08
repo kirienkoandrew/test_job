486d030f2139edcc3f36274cc686149201f3a682
kirienkoandrew <kir.andrew@gmail.com>
2022-02-08 17:48:21 +0700

    beta

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
-print('''Вас приветствует программа определения координат.
-Чтобы зайти в настройки нажмите 1
-Чтобы определить координаты, введите адрес
+print('''Вас приветствует программа определения точных координат введенного адреса.
 ''')
 
 from dadata import Dadata
 
-# import sqlite3
-#
-# connection = sqlite3.connect('settings.db')
-# cursor = connection.cursor()
-# cursor.execute('''CREATE TABLE IF NOT EXISTS Settings
-#               (token TEXT, secret TEXT, lang TEXT)''')
-#
-# cursor.execute(
-#     "INSERT INTO Settings VALUES ('Stranger Things', 'Shawn Levy', 2016)")
-# cursor.execute("SELECT * FROM Shows")
-#
-# print(cursor.fetchone())
-#
-# connection.commit()
-# connection.close()
-
-#создание таблицы
 import sqlite3
 
+# создание таблицы
 try:
     sqlite_connection = sqlite3.connect('settings.db')
-    sqlite_create_table_query = '''CREATE TABLE settings (token TEXT, secret TEXT, lang TEXT);'''
+    sqlite_create_table_query = '''CREATE TABLE settings (token TEXT, secret TEXT, lang TEXT DEFAULT ru, base_url TEXT DEFAULT https://dadata.ru/);'''
 
     cursor = sqlite_connection.cursor()
-    print("База данных подключена к SQLite")
+    # print("База данных подключена к SQLite")
     cursor.execute(sqlite_create_table_query)
     sqlite_connection.commit()
-    print("Таблица SQLite создана")
+    # print("Таблица SQLite создана")
 
     cursor.close()
 
@@ -41,20 +23,20 @@ except sqlite3.Error as error:
 finally:
     if (sqlite_connection):
         sqlite_connection.close()
-        print("Соединение с SQLite закрыто")
+        # print("Соединение с SQLite закрыто")
 
-#добавление данных в таблицу
+# добавление данных в таблицу
 try:
     sqlite_connection = sqlite3.connect('settings.db')
     cursor = sqlite_connection.cursor()
-    print("Подключен к SQLite")
-    token = input('Введите token')
-    secret = input('Введите secret')
-    lang = input('Введите lang')
+    # print("Подключен к SQLite")
+    token = input('Введите API-ключ: ')
+    secret = input('Введите секретный ключ: ')
+    lang = 'ru'
     sqlite_insert_query = f"INSERT INTO 'settings' (token, secret, lang) VALUES ('{token}', '{secret}', '{lang}');"
     count = cursor.execute(sqlite_insert_query)
     sqlite_connection.commit()
-    print("Запись успешно вставлена в таблицу settings ", cursor.rowcount)
+    # print("Запись успешно вставлена в таблицу settings ", cursor.rowcount)
     cursor.close()
 
 except sqlite3.Error as error:
@@ -62,23 +44,19 @@ except sqlite3.Error as error:
 finally:
     if sqlite_connection:
         sqlite_connection.close()
-        print("Соединение с SQLite закрыто")
+        # print("Соединение с SQLite закрыто")
 
-def read_sqlite_table(): # чтение строки из БД
+
+def read_sqlite_table():  # чтение строки из БД
     try:
         sqlite_connection = sqlite3.connect('settings.db')
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
+        # print("Подключен к SQLite")
 
         sqlite_select_query = """SELECT * from settings"""
         cursor.execute(sqlite_select_query)
         records = cursor.fetchall()
-        print("Всего строк:  ", len(records))
-        print(records)
-        # for row in records:
-        #     print("Token:", row[0])
-        #     print("Secret:", row[1])
-        #     print("Lang:", row[2], end="\n\n")
+
         token, secret, lang = records[0][0], records[0][1], records[0][2]
         cursor.close()
         return token, secret, lang
@@ -88,18 +66,38 @@ def read_sqlite_table(): # чтение строки из БД
     finally:
         if sqlite_connection:
             sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
+            # print("Соединение с SQLite закрыто")
 
-def delete_sqlite_record(): # очищаем БД
+
+def delete_sqlite_record():  # очищаем БД
     try:
         sqlite_connection = sqlite3.connect('settings.db')
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
-
-        #sql_update_query = """DELETE from settings where id = ?"""
-        cursor.execute('DELETE from settings;',)
+        # print("Подключен к SQLite")
+        cursor.execute('DELETE from settings;', )
         sqlite_connection.commit()
-        print("Запись успешно удалена")
+        # print("Запись успешно удалена")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            # print("Соединение с SQLite закрыто")
+
+
+def update_sqlite_table(new_lang):  # изменение языка в таблице
+    try:
+        sqlite_connection = sqlite3.connect('settings.db')
+        cursor = sqlite_connection.cursor()
+        #print("Подключен к SQLite")
+
+        sql_update_query = f"Update settings set lang = '{new_lang}'"
+
+        cursor.execute(sql_update_query)
+        sqlite_connection.commit()
+        print("Запись успешно обновлена")
         cursor.close()
 
     except sqlite3.Error as error:
@@ -110,56 +108,123 @@ def delete_sqlite_record(): # очищаем БД
             print("Соединение с SQLite закрыто")
 
 
+def update_sqlite_table_api(new_token, new_secret):  # изменения secret и token
+    try:
+        sqlite_connection = sqlite3.connect('settings.db')
+        cursor = sqlite_connection.cursor()
+        #print("Подключен к SQLite")
+        sql_update_query = f"Update settings set token = '{new_token}', secret = '{new_secret}'"
+        cursor.execute(sql_update_query)
+        sqlite_connection.commit()
+        print("Запись успешно обновлена")
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
 
-# token = input('Введите token')
-# secret = input('Введите secret')
+
+def update_sqlite_table_url(new_url):  # изменения URL
+    try:
+        sqlite_connection = sqlite3.connect('settings.db')
+        cursor = sqlite_connection.cursor()
+        #print("Подключен к SQLite")
+        sql_update_query = f"Update settings set base_url = '{new_url}'"
+        cursor.execute(sql_update_query)
+        sqlite_connection.commit()
+        print("Запись успешно обновлена")
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
+
+
 token, secret, lang = read_sqlite_table()
 dadata = Dadata(token, secret)
 
-print('Введите адрес: ')
-
-results = dadata.suggest(name="address", query='Ленина 8')
-
-print()
-
 
 def result():
+    token, secret, lang = read_sqlite_table()
+    print('Введите адрес: ')
+    results = dadata.suggest(name='address', query=input(), language=lang, count=20)
+
     if len(results) > 1:
+        print('Список адресов по Вашему запросу:')
         for i, result in enumerate(results):
-            print(i + 1, result)
+            print(f"{i + 1}. {result['value']}")
         print('Выберите нужный адрес: ')
         item = checker(results)
         while 1 > item or item > len(results):
             print('Введите число от 1 до ', len(results))
             item = checker(results)
         else:
-            print(results[item - 1])
-            print(results[item - 1]['data']['geo_lat'], results[item - 1]['data']['geo_lon'])
+            print(f"Координаты по адресу {results[item - 1]['value']}: ")
+            print(f"широта: {results[item - 1]['data']['geo_lat']}, долгота: {results[item - 1]['data']['geo_lon']}")
+
             one_more_time()
 
     elif len(results) == 0:
         print('Ничего не найдено')
         one_more_time()
     elif len(results) == 1:
-        print(results)
-        print(results[0]['data']['geo_lat'], results[0]['data']['geo_lon'])
+        print(f"Найден адрес {results[0]['value']}. Это то, что Вы искали?")
+        if input('да/нет ').lower() == 'да':
+            print(f"Координаты по адресу {results[0]['value']}: ")
+            print(f"широта: {results[0]['data']['geo_lat']}, долгота: {results[0]['data']['geo_lon']}")
+        else:
+            one_more_time()
+
         one_more_time()
 
 
 def one_more_time():
-    if input('Еще раз? да/нет  ').lower() == 'да':
+    res = input('Проверить другой адрес? да/нет. Для изменения настроек введите settings ')
+    if res.lower() == 'да':
         result()
+    elif res == 'settings':
+        print('''Что хотите изменить?
+1. Базовый URL к сервису dadata
+2. API ключ для сервиса dadata
+3. Язык, на котором должен возвращаться ответ от dadata
+Если передумали менять, введите 0
+''')
+        settings_input = int(input())
+        if settings_input == 1:
+            input_new_url = input('Введите новый URL: ')
+            update_sqlite_table_url(input_new_url)
+            one_more_time()
+        elif settings_input == 2:
+            input_token = input('Введите новый API-ключ: ')
+            input_secret = input('Введите новый секретный ключ: ')
+            update_sqlite_table_api(input_token, input_secret)
+            one_more_time()
+        elif settings_input == 3:
+            while True:
+                print('Введите ru или en')
+                item = input()
+                if item in ['ru', 'en']:
+                    update_sqlite_table(item)
+                    one_more_time()
+                else:
+                    print('Неверный ввод.')
+        else:
+            one_more_time()
     else:
         delete_sqlite_record()
         exit()
 
 
-def checker(results):
+def checker(results_list):
     while True:
         try:
             item = int(input())
         except:
-            print('Введите число от 1 до ', len(results))
+            print('Введите число от 1 до ', len(results_list))
         else:
             return item
 
